265e84e8b5023a399d4a223274358f2437050a3f
James Wong <jwyn0803@gmail.com>
2026-01-19 11:10:39 +0800

    added osgi implementation for user management and personal info

--- personal-info-impl/src/main/java/com/physiotrack/personal/info/impl/Activator.java
+++ personal-info-impl/src/main/java/com/physiotrack/personal/info/impl/Activator.java
@@ -1,23 +1,42 @@
 package com.physiotrack.personal.info.impl;
 
 import com.physiotrack.personal.info.api.PersonalInfoService;
+import com.physiotrack.user.management.api.UserManagementService;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
 
-    private ServiceRegistration<PersonalInfoService> reg;
+    private ServiceRegistration<PersonalInfoService> registration;
 
     @Override
-    public void start(BundleContext context) {
-        reg = context.registerService(PersonalInfoService.class, new PersonalInfoServiceImpl(), null);
-        System.out.println("[personal-info-impl] PersonalInfoService registered");
+    public void start(BundleContext context) throws Exception {
+        System.out.println("[personal-info-impl] Starting...");
+
+        // 1. Look up the UserManagementService
+        ServiceReference<UserManagementService> ref = context.getServiceReference(UserManagementService.class);
+        
+        if (ref != null) {
+            UserManagementService userMgr = context.getService(ref);
+
+            // 2. Create our service, injecting the dependency
+            PersonalInfoService personalInfoService = new PersonalInfoServiceImpl(userMgr);
+
+            // 3. Register our service
+            registration = context.registerService(PersonalInfoService.class, personalInfoService, null);
+            System.out.println("[personal-info-impl] PersonalInfoService registered");
+        } else {
+            System.err.println("[personal-info-impl] FAILED: UserManagementService not found!");
+        }
     }
 
     @Override
-    public void stop(BundleContext context) {
-        if (reg != null) reg.unregister();
-        System.out.println("[personal-info-impl] stopped");
+    public void stop(BundleContext context) throws Exception {
+        if (registration != null) {
+            registration.unregister();
+        }
+        System.out.println("[personal-info-impl] Stopped");
     }
 }
